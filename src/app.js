95fde951fe2453cabccccb1d03d3f7a01f56a986
IreneSaizdg <irenesaiz@idarte.eus>
2025-07-03 14:00:07 +0200

    ADD: app.js y actualizaciñon del .env

---
+++ src/app.js
@@ -0,0 +1,44 @@
+// IMPORTS
+const express = require('express') //Importa el framework
+require('dotenv').config(); //Carga las variables de entorno
+
+const app = express() //Instancia de express
+const cors = require('cors'); //CORS (mw)
+
+const port = process.env.PORT || 5000; //Configura el puerto
+
+
+
+
+// MIDDLEWARES: express.json express.URLencoded
+
+// MW:Parseo
+app.use(express.urlencoded({ extended: true})) //Parsear datos URL-encoded (formularios HTML)
+app.use(express.json()); //Parsear JSON en las peticiones
+
+// MW:Config de las CORS
+const frontUrlBase = process.env.FRONT_URL || "http://localhost:4000"
+const whiteList = [frontUrlBase]
+app.use(cors({
+    origin: whiteList //Peticiones desde dominios de la lista
+}))
+
+// MW: Logueo de peticiones entrantes
+app.use((req, res, next) => {
+    console.log(`➡️ ${req.method} ${req.originalUrl}`);
+    next();
+});
+
+// MD: MULTER Servir archivos estáticos
+app.use("/uploads", express.static("uploads")); //Esto permite acceder a imágenes o archivos subidos desde el navegador
+
+
+
+// RUTAS: 
+
+
+
+// INICIO DEL SERVIDOR 
+app.listen(port,() => {
+    console.log(`Server running on port: ${port}`)
+})
---
+++ src/queries/resources.queries.js
@@ -0,0 +1,7 @@
+//Insertar una relación de favorito entre un usuario y un recurso
+
+//Obtener el título de un recurso por un usuario específico
+
+//Obtener todas las pelis favs de un usuario y sus datos
+
+//Eliminar un favorito a partir del ID de usuario y de
